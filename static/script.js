ec37305106e9ae4362d0473237a647fe54de7dd7
giangdh <gianglibra1710@gmail.com>
2025-02-23 11:00:35 +0100

    read text

--- static/script.js
+++ static/script.js
@@ -16,7 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
         utterance.lang = 'en-US';  // You can change the language if needed (e.g., 'en-GB' for British English)
         window.speechSynthesis.speak(utterance);
     }
-    
+    readAloud("What is your full name?");
+
     // 👉 Load câu hỏi tiếp theo
     function fetchNextQuestion() {
         fetch(`/next_question?index=${currentIndex}`)
